350ba10ba7d98664b72c769ef3bbfb67f94c2d52
qedi0l <qediworks@gmail.com>
2024-06-30 04:15:05 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/request/delete/{UUID}', [RequestsController::class, 'delete'])->nam
 // Auth required
 Route::middleware('auth')->group(function () 
 {
-    // Accaunt
+    // Account
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
